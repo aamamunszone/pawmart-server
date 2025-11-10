a4c083e6c891e9dad73dc741daceb8d4ba7eb69f
Abdullah Al Mamun <aamamunszone@gmail.com>
2025-11-10 09:32:08 +0600

    feat(api): basic listings collection apis created.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // mongodb connection
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,11 +27,60 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const database = client.db('PawMart_DB');
+    const listingsCollection = database.collection('listings');
+
     //  ========== ROUTES START ==========
 
     // health check
     app.get('/', (req, res) => {
       res.send('PawMart Server is Running!');
+    });
+
+    // listings collection apis
+
+    // all listings get & specific user's listings (by email)
+    app.get('/listings', async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+      const cursor = listingsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // recent listings get (6)
+    app.get('/listings/recent', async (req, res) => {
+      const sortFields = { date: -1 };
+      const limitNum = 6;
+      const cursor = listingsCollection.find().sort(sortFields).limit(limitNum);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // specific listing get
+    app.get('/listings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await listingsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // new listing create
+    app.post('/listings', async (req, res) => {
+      const newListing = req.body;
+      const result = await listingsCollection.insertOne(newListing);
+      res.send(result);
+    });
+
+    // listing delete
+    app.delete('/listings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await listingsCollection.deleteOne(query);
+      res.send(result);
     });
 
     //  ========== ROUTES END ==========
